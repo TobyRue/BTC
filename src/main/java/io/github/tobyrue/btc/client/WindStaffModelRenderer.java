fa49d298bf6875f43e724724e7f2a2805483fe27
TobyRue <76403689+TobyRue@users.noreply.github.com>
2024-07-18 11:25:03 -0600

    wind staff

--- src/main/java/io/github/tobyrue/btc/client/WindStaffModelRenderer.java
+++ src/main/java/io/github/tobyrue/btc/client/WindStaffModelRenderer.java
@@ -1,5 +1,6 @@
 package io.github.tobyrue.btc.client;
 
+import io.github.tobyrue.btc.ModItems;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRenderer;
@@ -21,46 +22,30 @@ import net.minecraft.util.math.RotationAxis;
 @Environment(EnvType.CLIENT)
 public class WindStaffModelRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer {
 
-    /*@Override
-    public void render(ItemStack stack, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        matrices.push();
-        var tickDelta = MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(true);
-        double offset = Math.sin((MinecraftClient.getInstance().world.getTime() + tickDelta) / 8.0) / 8.0;
 
-        // Move the item
-        matrices.translate(0.5, 1.3 + offset, 0.5);
-
-        // Rotate the item
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees((MinecraftClient.getInstance().world.getTime() + tickDelta) * 4));
-        int lightAbove = WorldRenderer.getLightmapCoordinates(MinecraftClient.getInstance().world, MinecraftClient.getInstance().player.getBlockPos().up());
-
-        // Render the Wind Charge entity
-        WindChargeEntity windChargeEntity = new WindChargeEntity(EntityType.WIND_CHARGE, MinecraftClient.getInstance().world);
-        MinecraftClient.getInstance().getEntityRenderDispatcher().render(windChargeEntity, 0, 0, 0, 0.0f, tickDelta, matrices, vertexConsumers, lightAbove);
-        System.out.println("Render Wind");
-        // Mandatory call after GL calls
-        matrices.pop();
-    }*/
 
     @Override
     public void render(ItemStack stack, ModelTransformationMode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         matrices.push();
-        matrices.push();
-        var tickDelta = MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(true);
-        double offset = Math.sin((MinecraftClient.getInstance().world.getTime() + tickDelta) / 8.0) / 8.0;
+        /* var tickDelta = MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(false);
+        //double offset = Math.sin((MinecraftClient.getInstance().world.getTime() + tickDelta) / 8.0) / 8.0;
 
         // Move the item
-        matrices.translate(0.5, 1.3 + offset, 0.5);
+        matrices.translate(0.5, 1.3, 0.5);
 
         // Rotate the item
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees((MinecraftClient.getInstance().world.getTime() + tickDelta) * 4));
+        //matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees((MinecraftClient.getInstance().world.getTime() + tickDelta) * 4));
         int lightAbove = WorldRenderer.getLightmapCoordinates(MinecraftClient.getInstance().world, MinecraftClient.getInstance().player.getBlockPos().up());
 
         // Render the Wind Charge entity
         WindChargeEntity windChargeEntity = new WindChargeEntity(EntityType.WIND_CHARGE, MinecraftClient.getInstance().world);
         MinecraftClient.getInstance().getEntityRenderDispatcher().render(windChargeEntity, 0, 0, 0, 0.0f, tickDelta, matrices, vertexConsumers, lightAbove);
-        System.out.println("Render Wind");
         // Mandatory call after GL calls
+        */
+        var tickDelta = MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(false);
+
+        int lightAbove = WorldRenderer.getLightmapCoordinates(MinecraftClient.getInstance().world, MinecraftClient.getInstance().player.getBlockPos().up());
+
         matrices.pop();
     }
 }
