abace7b54c0c339e091d9f4f73adfea330d4edfc
TobyRue <76403689+TobyRue@users.noreply.github.com>
2024-07-26 17:59:30 -0600

    dungeons redstone wire rendering done
    add light emissions when powered = true

--- src/main/java/io/github/tobyrue/btc/DungeonWireBlock.java
+++ src/main/java/io/github/tobyrue/btc/DungeonWireBlock.java
@@ -3,12 +3,14 @@ package io.github.tobyrue.btc;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.mojang.serialization.MapCodec;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +19,12 @@ import java.util.Map;
 
 public class DungeonWireBlock extends Block {
     public static final MapCodec<DungeonWireBlock> CODEC = createCodec(DungeonWireBlock::new);
+    public static final BooleanProperty FACING_DOWN = BooleanProperty.of("facing_down");
+    public static final BooleanProperty FACING_UP = BooleanProperty.of("facing_up");
+    public static final BooleanProperty FACING_LEFT = BooleanProperty.of("facing_left");
+    public static final BooleanProperty FACING_RIGHT = BooleanProperty.of("facing_right");
+    public static final BooleanProperty POWERED = BooleanProperty.of("powered");
+    public static DirectionProperty FACING;
 
     public DungeonWireBlock(Settings settings) {
         super(settings);
@@ -25,6 +33,7 @@ public class DungeonWireBlock extends Block {
                 .with(FACING_UP, false)
                 .with(FACING_RIGHT, false)
                 .with(FACING_LEFT, false)
+                .with(FACING, Direction.NORTH)
                 .with(POWERED, false));
     }
 
@@ -32,11 +41,7 @@ public class DungeonWireBlock extends Block {
         return CODEC;
     }
 
-    public static final BooleanProperty FACING_DOWN = BooleanProperty.of("facing_down");
-    public static final BooleanProperty FACING_UP = BooleanProperty.of("facing_up");
-    public static final BooleanProperty FACING_LEFT = BooleanProperty.of("facing_left");
-    public static final BooleanProperty FACING_RIGHT = BooleanProperty.of("facing_right");
-    public static final BooleanProperty POWERED = BooleanProperty.of("powered");
+
 
 
     @Nullable
@@ -47,12 +52,14 @@ public class DungeonWireBlock extends Block {
                 .with(FACING_UP, false)
                 .with(FACING_LEFT, false)
                 .with(FACING_RIGHT, false)
-
-                .with(POWERED, ctx.getWorld().isReceivingRedstonePower(ctx.getBlockPos()));
+                .with(POWERED, ctx.getWorld().isReceivingRedstonePower(ctx.getBlockPos()))
+                .with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
     }
-
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING_DOWN, FACING_UP, FACING_LEFT, FACING_RIGHT, POWERED);
+        builder.add(FACING_DOWN, FACING_UP, FACING_LEFT, FACING_RIGHT, FACING, POWERED);
+    }
+    static {
+        FACING = HorizontalFacingBlock.FACING;
     }
 }
