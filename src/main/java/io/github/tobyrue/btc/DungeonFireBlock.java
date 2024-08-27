d8d26cf4acc572db51ae3a148da58265e0346c04
TobyRue <76403689+TobyRue@users.noreply.github.com>
2024-08-26 20:38:46 -0600

    functional

--- src/main/java/io/github/tobyrue/btc/DungeonFireBlock.java
+++ src/main/java/io/github/tobyrue/btc/DungeonFireBlock.java
@@ -1,18 +1,40 @@
 package io.github.tobyrue.btc;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.CampfireBlock;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageSources;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class DungeonFireBlock extends Block {
+    public static final IntProperty DAMAGE = IntProperty.of("damage", 0, 31);
     private static final VoxelShape SHAPE = Block.createCuboidShape(2.0, 0.0, 2.0, 14.0, 12.0, 14.0);
+
     public DungeonFireBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState()
+                .with(DAMAGE, 2));
+    }
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState()
+                .with(DAMAGE, 2);
+    }
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(DAMAGE);
     }
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
@@ -28,4 +50,16 @@ public class DungeonFireBlock extends Block {
     public VoxelShape getRaycastShape(BlockState state, BlockView world, BlockPos pos) {
         return SHAPE;
     }
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+            if (!entity.bypassesSteppingEffects() && entity instanceof LivingEntity) {
+                int damageValue = state.get(DAMAGE);
+                // Convert the damage value to float
+                float damageFloat = (float) damageValue;
+                entity.damage(world.getDamageSources().hotFloor(), damageFloat);
+            }
+
+
+        super.onSteppedOn(world, pos, state, entity);
+    }
 }
+
--- src/main/java/io/github/tobyrue/btc/ModDamageTypes.java
+++ src/main/java/io/github/tobyrue/btc/ModDamageTypes.java
@@ -1,6 +1,8 @@
 package io.github.tobyrue.btc;
 
 import net.minecraft.entity.damage.*;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -9,6 +11,7 @@ import net.minecraft.world.World;
 public class ModDamageTypes {
 
     static RegistryKey<DamageType> BEACON_BURN = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of("btc", "beacon_burn"));
+    static RegistryKey<DamageType> FIRE_BURN = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of("btc", "fire_burn"));
 
     public static DamageSource of(World world, RegistryKey<DamageType> key) {
         return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
