1e928f877fbd6762e0f83b3348a522eb3a7d850f
TobyRue <76403689+TobyRue@users.noreply.github.com>
2025-11-06 21:22:25 -0700

    save partical animation type

--- src/main/java/io/github/tobyrue/btc/spell/ChanneledSpell.java
+++ src/main/java/io/github/tobyrue/btc/spell/ChanneledSpell.java
@@ -2,17 +2,12 @@ package io.github.tobyrue.btc.spell;
 
 import io.github.tobyrue.btc.Ticker;
 import io.github.tobyrue.btc.enums.SpellTypes;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.projectile.SmallFireballEntity;
-import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.particle.EntityEffectParticleEffect;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.joml.Vector3f;
 
 public abstract class ChanneledSpell extends Spell {
     private final int castTime;
@@ -20,14 +15,31 @@ public abstract class ChanneledSpell extends Spell {
     private final boolean canBeDisturbed;
     private final boolean showParticles;
     private final ParticleEffect particleType;
+    private final ParticleAnimation animation;
 
-    public ChanneledSpell(SpellTypes type, int castTime, int intervalTicks, boolean canBeDisturbed, boolean showParticles, ParticleEffect particleType) {
+    public enum ParticleAnimation {
+        SPIRAL,
+        CYLINDER
+    }
+
+    public ChanneledSpell(SpellTypes type, int castTime, int intervalTicks, boolean canBeDisturbed, boolean showParticles, ParticleEffect particleType, ParticleAnimation animation) {
         super(type);
         this.castTime = castTime;
         this.intervalTicks = intervalTicks;
         this.canBeDisturbed = canBeDisturbed;
         this.showParticles = showParticles;
         this.particleType = particleType;
+        this.animation = animation;
+    }
+
+    public ChanneledSpell(SpellTypes type, int castTime, int intervalTicks, boolean canBeDisturbed) {
+        super(type);
+        this.castTime = castTime;
+        this.intervalTicks = intervalTicks;
+        this.canBeDisturbed = canBeDisturbed;
+        this.showParticles = false;
+        this.particleType = ParticleTypes.ENCHANTED_HIT;
+        this.animation = ParticleAnimation.CYLINDER;
     }
 
     @Override
@@ -50,7 +62,11 @@ public abstract class ChanneledSpell extends Spell {
                         }
                     }
                     if (showParticles) {
-                        spawnChannelParticles(ctx.user(), tick, castTime, args);
+                        if (animation == ParticleAnimation.CYLINDER) {
+                            spawnChannelParticlesCylinder(ctx.user(), tick, castTime, args);
+                        } else {
+
+                        }
                     }
                     return false;
                 }, castTime)
@@ -59,7 +75,7 @@ public abstract class ChanneledSpell extends Spell {
 
     protected abstract void useChanneled(final SpellContext ctx, final GrabBag args, final int tick);
 
-    protected final void spawnChannelParticles(LivingEntity entity, int tick, int totalDuration, GrabBag args) {
+    protected final void spawnChannelParticlesCylinder(LivingEntity entity, int tick, int totalDuration, GrabBag args) {
         World world = entity.getWorld();
         if (world.isClient) return;
 
@@ -109,6 +125,59 @@ public abstract class ChanneledSpell extends Spell {
                 } else {
                     world.addParticle(particleType,
                             x, y, z, 0, 0, 0);
+                }
+            }
+        }
+    }
+    protected final void spawnChannelParticlesSpiral(LivingEntity entity, int tick, int totalDuration, GrabBag args) {
+        World world = entity.getWorld();
+        if (world.isClient) return;
+
+        double height = entity.getHeight();
+        double width = entity.getWidth();
+        double radius = width * 0.75;
+
+        int steps = args.getInt("particleCount", 40); // number of points per spiral rotation
+        int rotations = args.getInt("spiralRotations", 3); // how many full turns it makes
+
+        // progress (0–1) through the entire duration
+        double progress = (double) tick / totalDuration;
+
+        // total height reached so far
+        double yProgress = progress * height;
+
+        // how much to advance angle and height per particle
+        double angleStep = (2 * Math.PI * rotations) / steps;
+        double heightStep = yProgress / steps;
+
+        float r = ((getColor(args) >> 16) & 0xFF) / 255.0F;
+        float g = ((getColor(args) >> 8) & 0xFF) / 255.0F;
+        float b = (getColor(args) & 0xFF) / 255.0F;
+
+        for (int i = 0; i < steps; i++) {
+            double theta = i * angleStep + (tick * 0.2); // spin continuously
+            double y = entity.getY() + (i * heightStep); // climb as it spins
+            double x = entity.getX() + Math.cos(theta) * radius;
+            double z = entity.getZ() + Math.sin(theta) * radius;
+
+            if (particleType == null) {
+                // tintable fallback (magic swirl)
+                if (world instanceof ServerWorld server) {
+                    server.spawnParticles(
+                            EntityEffectParticleEffect.create(ParticleTypes.ENTITY_EFFECT, r, g, b),
+                            x, y, z, 1, 0, 0, 0, 0
+                    );
+                } else {
+                    world.addParticle(
+                            EntityEffectParticleEffect.create(ParticleTypes.ENTITY_EFFECT, r, g, b),
+                            x, y, z, 0, 0, 0
+                    );
+                }
+            } else {
+                if (world instanceof ServerWorld server) {
+                    server.spawnParticles(particleType, x, y, z, 1, 0, 0, 0, 0);
+                } else {
+                    world.addParticle(particleType, x, y, z, 0, 0, 0);
                 }
             }
         }
