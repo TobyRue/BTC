1b37bec9196b278ac402e478b18254bb9c13fc33
TobyRue <76403689+TobyRue@users.noreply.github.com>
2026-06-20 23:15:34 -0600

    .

--- src/main/java/io/github/tobyrue/btc/datagen/ModBlockLootTableProvider.java
+++ src/main/java/io/github/tobyrue/btc/datagen/ModBlockLootTableProvider.java
@@ -14,54 +14,6 @@ public class ModBlockLootTableProvider extends FabricBlockLootTableProvider {
 
     @Override
     public void generate() {
-        addDrop(ModBlocks.GUNPOWDER_BARREL);
-        addDrop(ModBlocks.KILL_BALL_RECEPTOR);
-        addDrop(ModBlocks.BELLOW);
-        addDrop(ModBlocks.BRAZIER);
-        addDrop(ModBlocks.OMINOUS_BEACON);
-        addDrop(ModBlocks.MOB_DETECTOR);
-        addDrop(ModBlocks.DUNGEON_PRESSURE_PLATE);
-        addDrop(ModBlocks.POLISHED_TUFF_PRESSURE_PLATE);
-        addDrop(ModBlocks.DUNGEON_BUTTON);
-        addDrop(ModBlocks.PEDESTAL);
-        addDrop(ModBlocks.KEY_DISPENSER_BLOCK);
-        addDrop(ModBlocks.KEY_ACCEPTOR);
-        addDrop(ModBlocks.DUNGEON_DOOR);
-        addDrop(ModBlocks.DUNGEON_FLAME);
-        addDrop(ModBlocks.DEEP_FLAME);
-        addDrop(ModBlocks.FORTRESS_FLAME);
 
-        addDrop(ModBlocks.UNOXIDIZED_COPPER_BUTTON);
-        addDrop(ModBlocks.WAXED_UNOXIDIZED_COPPER_BUTTON);
-        addDrop(ModBlocks.EXPOSED_COPPER_BUTTON);
-        addDrop(ModBlocks.WAXED_EXPOSED_COPPER_BUTTON);
-        addDrop(ModBlocks.WEATHERED_COPPER_BUTTON);
-        addDrop(ModBlocks.WAXED_WEATHERED_COPPER_BUTTON);
-        addDrop(ModBlocks.OXIDIZED_COPPER_BUTTON);
-        addDrop(ModBlocks.WAXED_OXIDIZED_COPPER_BUTTON);
-
-        addDrop(ModBlocks.COPPER_TRIAL_FAN);
-        addDrop(ModBlocks.EXPOSED_COPPER_TRIAL_FAN);
-        addDrop(ModBlocks.WEATHERED_COPPER_TRIAL_FAN);
-        addDrop(ModBlocks.OXIDIZED_COPPER_TRIAL_FAN);
-        addDrop(ModBlocks.WAXED_COPPER_TRIAL_FAN);
-        addDrop(ModBlocks.WAXED_EXPOSED_COPPER_TRIAL_FAN);
-        addDrop(ModBlocks.WAXED_WEATHERED_COPPER_TRIAL_FAN);
-        addDrop(ModBlocks.WAXED_OXIDIZED_COPPER_TRIAL_FAN);
-
-        addDrop(ModBlocks.CHISELED_TUFF_BRICKS_PILLAR);
-        addDrop(ModBlocks.TUFF_PILLAR);
-        addDrop(ModBlocks.STONE_PILLAR);
-        addDrop(ModBlocks.CRACKED_STONE_BRICKS_PILLAR);
-        addDrop(ModBlocks.STONE_BRICKS_PILLAR);
-        addDrop(ModBlocks.POLISHED_TUFF_PILLAR);
-        addDrop(ModBlocks.TUFF_BRICKS_PILLAR);
-        addDrop(ModBlocks.STONE_PILASTER);
-        addDrop(ModBlocks.STONE_BRICKS_PILASTER);
-        addDrop(ModBlocks.CRACKED_STONE_BRICKS_PILASTER);
-        addDrop(ModBlocks.TUFF_BRICK_PILASTER);
-        addDrop(ModBlocks.CHISELED_TUFF_BRICKS_PILASTER);
-        addDrop(ModBlocks.POLISHED_TUFF_PILASTER);
-        addDrop(ModBlocks.TUFF_PILASTER);
     }
 }
