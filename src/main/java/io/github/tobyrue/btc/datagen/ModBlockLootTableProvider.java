24915f31a213f5d20148332536505cf6763ef1f4
TobyRue <76403689+TobyRue@users.noreply.github.com>
2026-07-25 12:21:24 -0600

    recipies and loot tables

--- src/main/java/io/github/tobyrue/btc/datagen/ModBlockLootTableProvider.java
+++ src/main/java/io/github/tobyrue/btc/datagen/ModBlockLootTableProvider.java
@@ -4,8 +4,8 @@ import io.github.tobyrue.btc.block.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.minecraft.block.Block;
-import net.minecraft.item.ItemConvertible;
 import net.minecraft.registry.RegistryWrapper;
+
 import java.util.concurrent.CompletableFuture;
 
 public class ModBlockLootTableProvider extends FabricBlockLootTableProvider {
@@ -16,10 +16,44 @@ public class ModBlockLootTableProvider extends FabricBlockLootTableProvider {
 
     @Override
     public void generate() {
-        addDrop(ModBlocks.CHARGED_COPPER, ModBlocks.EXPOSED_CHARGED_COPPER,
-                ModBlocks.WEATHERED_CHARGED_COPPER, ModBlocks.OXIDIZED_CHARGED_COPPER,
-                ModBlocks.WAXED_CHARGED_COPPER, ModBlocks.WAXED_EXPOSED_CHARGED_COPPER,
-                ModBlocks.WAXED_WEATHERED_CHARGED_COPPER, ModBlocks.WAXED_OXIDIZED_CHARGED_COPPER);
+        addDrop(
+                ModBlocks.TUFF_PILASTER,
+                ModBlocks.TUFF_PILLAR,
+                ModBlocks.TUFF_BRICK_PILASTER,
+                ModBlocks.TUFF_BRICKS_PILLAR,
+                ModBlocks.CHISELED_TUFF_BRICKS_PILASTER,
+                ModBlocks.CHISELED_TUFF_BRICKS_PILLAR,
+                ModBlocks.POLISHED_TUFF_PILASTER,
+                ModBlocks.POLISHED_TUFF_PILLAR,
+                ModBlocks.STONE_PILASTER,
+                ModBlocks.STONE_PILLAR,
+                ModBlocks.COBBLESTONE_PILASTER,
+                ModBlocks.COBBLESTONE_PILLAR,
+                ModBlocks.MOSSY_COBBLESTONE_PILASTER,
+                ModBlocks.MOSSY_COBBLESTONE_PILLAR,
+                ModBlocks.STONE_BRICKS_PILASTER,
+                ModBlocks.STONE_BRICKS_PILLAR,
+                ModBlocks.MOSSY_STONE_BRICKS_PILASTER,
+                ModBlocks.MOSSY_STONE_BRICKS_PILLAR,
+                ModBlocks.CRACKED_STONE_BRICKS_PILASTER,
+                ModBlocks.CRACKED_STONE_BRICKS_PILLAR,
+                ModBlocks.DEEPSLATE_PILASTER,
+                ModBlocks.DEEPSLATE_PILLAR,
+                ModBlocks.COBBLED_DEEPSLATE_PILASTER,
+                ModBlocks.COBBLED_DEEPSLATE_PILLAR,
+                ModBlocks.POLISHED_DEEPSLATE_PILASTER,
+                ModBlocks.POLISHED_DEEPSLATE_PILLAR,
+                ModBlocks.DEEPSLATE_BRICKS_PILASTER,
+                ModBlocks.DEEPSLATE_BRICKS_PILLAR,
+                ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILASTER,
+                ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILLAR,
+                ModBlocks.DEEPSLATE_TILES_PILASTER,
+                ModBlocks.DEEPSLATE_TILES_PILLAR,
+                ModBlocks.CRACKED_DEEPSLATE_TILES_PILASTER,
+                ModBlocks.CRACKED_DEEPSLATE_TILES_PILLAR,
+                ModBlocks.CHISELED_DEEPSLATE_PILASTER,
+                ModBlocks.CHISELED_DEEPSLATE_PILLAR
+        );
     }
 
     public void addDrop(Block... blocks) {
--- src/main/java/io/github/tobyrue/btc/datagen/ModRecipeProvider.java
+++ src/main/java/io/github/tobyrue/btc/datagen/ModRecipeProvider.java
@@ -4,13 +4,16 @@ import io.github.tobyrue.btc.BTC;
 import io.github.tobyrue.btc.block.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
-import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.server.recipe.*;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
@@ -22,85 +25,109 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     @Override
     public void generate(RecipeExporter exporter) {
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.CHARGED_COPPER)
-                .pattern("CCC")
-                .pattern("CRC")
-                .pattern("CCC")
-                .input('C', Items.COPPER_INGOT)
-                .input('R', Items.REDSTONE_BLOCK)
-                .criterion(hasItem(Items.COPPER_INGOT), conditionsFromItem(Items.COPPER_INGOT))
-                .criterion(hasItem(Items.REDSTONE_BLOCK), conditionsFromItem(Items.REDSTONE_BLOCK))
-                .offerTo(exporter, BTC.identifierOf("charged_copper_from_ingot"));
+        offerPillarSet(exporter, Blocks.TUFF, ModBlocks.TUFF_PILLAR, ModBlocks.TUFF_PILASTER);
+        offerPillarSet(exporter, Blocks.TUFF_BRICKS, ModBlocks.TUFF_BRICKS_PILLAR, ModBlocks.TUFF_BRICK_PILASTER);
+        offerPillarSet(exporter, Blocks.CHISELED_TUFF_BRICKS, ModBlocks.CHISELED_TUFF_BRICKS_PILLAR, ModBlocks.CHISELED_TUFF_BRICKS_PILASTER);
+        offerPillarSet(exporter, Blocks.POLISHED_TUFF, ModBlocks.POLISHED_TUFF_PILLAR, ModBlocks.POLISHED_TUFF_PILASTER);
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.CHARGED_COPPER)
-                .input(Items.COPPER_BLOCK)
-                .input(Items.REDSTONE_BLOCK)
-                .criterion(hasItem(Items.COPPER_BLOCK), conditionsFromItem(Items.COPPER_BLOCK))
-                .offerTo(exporter, BTC.identifierOf("charged_copper_from_block"));
+        offerPillarSet(exporter, Blocks.STONE, ModBlocks.STONE_PILLAR, ModBlocks.STONE_PILASTER);
+        offerPillarSet(exporter, Blocks.COBBLESTONE, ModBlocks.COBBLESTONE_PILLAR, ModBlocks.COBBLESTONE_PILASTER);
+        offerPillarSet(exporter, Blocks.MOSSY_COBBLESTONE, ModBlocks.MOSSY_COBBLESTONE_PILLAR, ModBlocks.MOSSY_COBBLESTONE_PILASTER);
+        offerPillarSet(exporter, Blocks.STONE_BRICKS, ModBlocks.STONE_BRICKS_PILLAR, ModBlocks.STONE_BRICKS_PILASTER);
+        offerPillarSet(exporter, Blocks.MOSSY_STONE_BRICKS, ModBlocks.MOSSY_STONE_BRICKS_PILLAR, ModBlocks.MOSSY_STONE_BRICKS_PILASTER);
+        offerPillarSet(exporter, Blocks.CRACKED_STONE_BRICKS, ModBlocks.CRACKED_STONE_BRICKS_PILLAR, ModBlocks.CRACKED_STONE_BRICKS_PILASTER);
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.EXPOSED_CHARGED_COPPER)
-                .input(Items.EXPOSED_COPPER)
-                .input(Items.REDSTONE_BLOCK)
-                .criterion(hasItem(Items.EXPOSED_COPPER), conditionsFromItem(Items.EXPOSED_COPPER))
-                .offerTo(exporter);
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WEATHERED_CHARGED_COPPER)
-                .input(Items.WEATHERED_COPPER)
-                .input(Items.REDSTONE_BLOCK)
-                .criterion(hasItem(Items.WEATHERED_COPPER), conditionsFromItem(Items.WEATHERED_COPPER))
-                .offerTo(exporter);
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.OXIDIZED_CHARGED_COPPER)
-                .input(Items.OXIDIZED_COPPER)
-                .input(Items.REDSTONE_BLOCK)
-                .criterion(hasItem(Items.OXIDIZED_COPPER), conditionsFromItem(Items.OXIDIZED_COPPER))
-                .offerTo(exporter);
+        offerPillarSet(exporter, Blocks.DEEPSLATE, ModBlocks.DEEPSLATE_PILLAR, ModBlocks.DEEPSLATE_PILASTER);
+        offerPillarSet(exporter, Blocks.COBBLED_DEEPSLATE, ModBlocks.COBBLED_DEEPSLATE_PILLAR, ModBlocks.COBBLED_DEEPSLATE_PILASTER);
+        offerPillarSet(exporter, Blocks.POLISHED_DEEPSLATE, ModBlocks.POLISHED_DEEPSLATE_PILLAR, ModBlocks.POLISHED_DEEPSLATE_PILASTER);
+        offerPillarSet(exporter, Blocks.DEEPSLATE_BRICKS, ModBlocks.DEEPSLATE_BRICKS_PILLAR, ModBlocks.DEEPSLATE_BRICKS_PILASTER);
+        offerPillarSet(exporter, Blocks.CRACKED_DEEPSLATE_BRICKS, ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILLAR, ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILASTER);
+        offerPillarSet(exporter, Blocks.DEEPSLATE_TILES, ModBlocks.DEEPSLATE_TILES_PILLAR, ModBlocks.DEEPSLATE_TILES_PILASTER);
+        offerPillarSet(exporter, Blocks.CRACKED_DEEPSLATE_TILES, ModBlocks.CRACKED_DEEPSLATE_TILES_PILLAR, ModBlocks.CRACKED_DEEPSLATE_TILES_PILASTER);
+        offerPillarSet(exporter, Blocks.CHISELED_DEEPSLATE, ModBlocks.CHISELED_DEEPSLATE_PILLAR, ModBlocks.CHISELED_DEEPSLATE_PILASTER);
 
+        offerStonecutting(exporter, Blocks.STONE, ModBlocks.STONE_PILLAR, ModBlocks.STONE_PILASTER);
+        offerStonecutting(exporter, Blocks.STONE_BRICKS, ModBlocks.STONE_BRICKS_PILLAR, ModBlocks.STONE_BRICKS_PILASTER);
+        offerStonecutting(exporter, Blocks.COBBLESTONE, ModBlocks.COBBLESTONE_PILLAR, ModBlocks.COBBLESTONE_PILASTER);
+        offerStonecutting(exporter, Blocks.MOSSY_COBBLESTONE, ModBlocks.MOSSY_COBBLESTONE_PILLAR, ModBlocks.MOSSY_COBBLESTONE_PILASTER);
+        offerStonecutting(exporter, Blocks.MOSSY_STONE_BRICKS, ModBlocks.MOSSY_STONE_BRICKS_PILLAR, ModBlocks.MOSSY_STONE_BRICKS_PILASTER);
+        offerStonecutting(exporter, Blocks.CRACKED_STONE_BRICKS, ModBlocks.CRACKED_STONE_BRICKS_PILLAR, ModBlocks.CRACKED_STONE_BRICKS_PILASTER);
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WAXED_CHARGED_COPPER)
-                .input(Items.WAXED_COPPER_BLOCK)
-                .input(Items.REDSTONE_BLOCK)
-                .criterion(hasItem(Items.WAXED_COPPER_BLOCK), conditionsFromItem(Items.WAXED_COPPER_BLOCK))
-                .offerTo(exporter, BTC.identifierOf("waxed_charged_copper_from_redstone"));
+        createStonecuttingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.STONE_BRICKS_PILLAR, Blocks.STONE, 4);
+        createStonecuttingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.STONE_BRICKS_PILASTER, Blocks.STONE, 1);
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WAXED_EXPOSED_CHARGED_COPPER)
-                .input(Items.WAXED_EXPOSED_COPPER)
-                .input(Items.REDSTONE_BLOCK)
-                .criterion(hasItem(Items.WAXED_EXPOSED_COPPER), conditionsFromItem(Items.WAXED_EXPOSED_COPPER))
-                .offerTo(exporter, BTC.identifierOf("waxed_exposed_charged_copper_from_redstone"));
+        offerStonecutting(exporter, Blocks.TUFF, ModBlocks.TUFF_PILLAR, ModBlocks.TUFF_PILASTER);
+        offerStonecutting(exporter, Blocks.TUFF_BRICKS, ModBlocks.TUFF_BRICKS_PILLAR, ModBlocks.TUFF_BRICK_PILASTER);
+        offerStonecutting(exporter, Blocks.CHISELED_TUFF_BRICKS, ModBlocks.CHISELED_TUFF_BRICKS_PILLAR, ModBlocks.CHISELED_TUFF_BRICKS_PILASTER);
+        offerStonecutting(exporter, Blocks.POLISHED_TUFF, ModBlocks.POLISHED_TUFF_PILLAR, ModBlocks.POLISHED_TUFF_PILASTER);
+        createStonecuttingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.TUFF_BRICKS_PILLAR, Blocks.TUFF, 4);
+        createStonecuttingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.TUFF_BRICK_PILASTER, Blocks.TUFF, 1);
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WAXED_WEATHERED_CHARGED_COPPER)
-                .input(Items.WAXED_WEATHERED_COPPER)
-                .input(Items.REDSTONE_BLOCK)
-                .criterion(hasItem(Items.WAXED_WEATHERED_COPPER), conditionsFromItem(Items.WAXED_WEATHERED_COPPER))
-                .offerTo(exporter, BTC.identifierOf("waxed_weathered_charged_copper_from_redstone"));
+        offerStonecutting(exporter, Blocks.DEEPSLATE, ModBlocks.DEEPSLATE_PILLAR, ModBlocks.DEEPSLATE_PILASTER);
+        offerStonecutting(exporter, Blocks.COBBLED_DEEPSLATE, ModBlocks.COBBLED_DEEPSLATE_PILLAR, ModBlocks.COBBLED_DEEPSLATE_PILASTER);
+        offerStonecutting(exporter, Blocks.POLISHED_DEEPSLATE, ModBlocks.POLISHED_DEEPSLATE_PILLAR, ModBlocks.POLISHED_DEEPSLATE_PILASTER);
+        offerStonecutting(exporter, Blocks.DEEPSLATE_BRICKS, ModBlocks.DEEPSLATE_BRICKS_PILLAR, ModBlocks.DEEPSLATE_BRICKS_PILASTER);
+        offerStonecutting(exporter, Blocks.CRACKED_DEEPSLATE_BRICKS, ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILLAR, ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILASTER);
+        offerStonecutting(exporter, Blocks.DEEPSLATE_TILES, ModBlocks.DEEPSLATE_TILES_PILLAR, ModBlocks.DEEPSLATE_TILES_PILASTER);
+        offerStonecutting(exporter, Blocks.CRACKED_DEEPSLATE_TILES, ModBlocks.CRACKED_DEEPSLATE_TILES_PILLAR, ModBlocks.CRACKED_DEEPSLATE_TILES_PILASTER);
+        offerStonecutting(exporter, Blocks.CHISELED_DEEPSLATE, ModBlocks.CHISELED_DEEPSLATE_PILLAR, ModBlocks.CHISELED_DEEPSLATE_PILASTER);
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WAXED_OXIDIZED_CHARGED_COPPER)
-                .input(Items.WAXED_OXIDIZED_COPPER)
-                .input(Items.REDSTONE_BLOCK)
-                .criterion(hasItem(Items.WAXED_OXIDIZED_COPPER), conditionsFromItem(Items.WAXED_OXIDIZED_COPPER))
-                .offerTo(exporter, BTC.identifierOf("waxed_oxidized_charged_copper_from_redstone"));
+        offerSmelting(exporter, List.of(ModBlocks.STONE_BRICKS_PILLAR), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_STONE_BRICKS_PILLAR, 0.1f, 200, "cracked_stone_bricks_pillar");
+        offerSmelting(exporter, List.of(ModBlocks.STONE_BRICKS_PILASTER), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_STONE_BRICKS_PILASTER, 0.1f, 200, "cracked_stone_bricks_pilaster");
+        offerBlasting(exporter, List.of(ModBlocks.STONE_BRICKS_PILLAR), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_STONE_BRICKS_PILLAR, 0.1f, 100, "cracked_stone_bricks_pillar");
+        offerBlasting(exporter, List.of(ModBlocks.STONE_BRICKS_PILASTER), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_STONE_BRICKS_PILASTER, 0.1f, 100, "cracked_stone_bricks_pilaster");
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WAXED_CHARGED_COPPER)
-                .input(ModBlocks.CHARGED_COPPER)
-                .input(Items.HONEYCOMB)
-                .criterion(hasItem(Items.HONEYCOMB), conditionsFromItem(Items.HONEYCOMB))
-                .offerTo(exporter);
+        offerSmelting(exporter, List.of(ModBlocks.DEEPSLATE_BRICKS_PILLAR), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILLAR, 0.1f, 200, "cracked_deepslate_bricks_pillar");
+        offerSmelting(exporter, List.of(ModBlocks.DEEPSLATE_BRICKS_PILASTER), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILASTER, 0.1f, 200, "cracked_deepslate_bricks_pilaster");
+        offerBlasting(exporter, List.of(ModBlocks.DEEPSLATE_BRICKS_PILLAR), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILLAR, 0.1f, 100, "cracked_deepslate_bricks_pillar");
+        offerBlasting(exporter, List.of(ModBlocks.DEEPSLATE_BRICKS_PILASTER), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_DEEPSLATE_BRICKS_PILASTER, 0.1f, 100, "cracked_deepslate_bricks_pilaster");
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WAXED_EXPOSED_CHARGED_COPPER)
-                .input(ModBlocks.EXPOSED_CHARGED_COPPER)
-                .input(Items.HONEYCOMB)
-                .criterion(hasItem(Items.HONEYCOMB), conditionsFromItem(Items.HONEYCOMB))
-                .offerTo(exporter);
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WAXED_WEATHERED_CHARGED_COPPER)
-                .input(ModBlocks.WEATHERED_CHARGED_COPPER)
-                .input(Items.HONEYCOMB)
-                .criterion(hasItem(Items.HONEYCOMB), conditionsFromItem(Items.HONEYCOMB))
-                .offerTo(exporter);
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WAXED_OXIDIZED_CHARGED_COPPER)
-                .input(ModBlocks.OXIDIZED_CHARGED_COPPER)
-                .input(Items.HONEYCOMB)
-                .criterion(hasItem(Items.HONEYCOMB), conditionsFromItem(Items.HONEYCOMB))
+        offerSmelting(exporter, List.of(ModBlocks.DEEPSLATE_TILES_PILLAR), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_DEEPSLATE_TILES_PILLAR, 0.1f, 200, "cracked_deepslate_tiles_pillar");
+        offerSmelting(exporter, List.of(ModBlocks.DEEPSLATE_TILES_PILASTER), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_DEEPSLATE_TILES_PILASTER, 0.1f, 200, "cracked_deepslate_tiles_pilaster");
+        offerBlasting(exporter, List.of(ModBlocks.DEEPSLATE_TILES_PILLAR), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_DEEPSLATE_TILES_PILLAR, 0.1f, 100, "cracked_deepslate_tiles_pillar");
+        offerBlasting(exporter, List.of(ModBlocks.DEEPSLATE_TILES_PILASTER), RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRACKED_DEEPSLATE_TILES_PILASTER, 0.1f, 100, "cracked_deepslate_tiles_pilaster");
+
+        offerMossyVariant(exporter, ModBlocks.COBBLESTONE_PILLAR, ModBlocks.MOSSY_COBBLESTONE_PILLAR);
+        offerMossyVariant(exporter, ModBlocks.COBBLESTONE_PILASTER, ModBlocks.MOSSY_COBBLESTONE_PILASTER);
+
+        offerMossyVariant(exporter, ModBlocks.STONE_BRICKS_PILLAR, ModBlocks.MOSSY_STONE_BRICKS_PILLAR);
+        offerMossyVariant(exporter, ModBlocks.STONE_BRICKS_PILASTER, ModBlocks.MOSSY_STONE_BRICKS_PILASTER);
+    }
+
+    private void offerPillarSet(RecipeExporter exporter, ItemConvertible input, Block pillar, Block pilaster) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, pillar, 8)
+                .pattern("B")
+                .pattern("B")
+                .input('B', input)
+                .criterion(hasItem(input), conditionsFromItem(input))
                 .offerTo(exporter);
 
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, pilaster, 6)
+                .pattern(" B ")
+                .pattern("BBB")
+                .input('B', input)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .offerTo(exporter);
+    }
 
+    private void offerStonecutting(RecipeExporter exporter, ItemConvertible input, Block pillar, Block pilaster) {
+        createStonecuttingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, pillar, input, 4);
+        createStonecuttingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, pilaster, input, 1);
+    }
+
+    private void createStonecuttingRecipe(RecipeExporter exporter, RecipeCategory category, ItemConvertible output, ItemConvertible input, int count) {
+        StonecuttingRecipeJsonBuilder.createStonecutting(Ingredient.ofItems(input), category, output, count)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .offerTo(exporter, RecipeProvider.getItemPath(output) + "_from_" + RecipeProvider.getItemPath(input) + "_stonecutting");
+    }
+
+    private void offerMossyVariant(RecipeExporter exporter, Block base, Block mossyResult) {
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, mossyResult)
+                .input(base)
+                .input(Ingredient.ofItems(Blocks.VINE, Blocks.MOSS_BLOCK))
+                .criterion(hasItem(base), conditionsFromItem(base))
+                .criterion(hasItem(Blocks.VINE), conditionsFromItem(Blocks.VINE))
+                .criterion(hasItem(Blocks.MOSS_BLOCK), conditionsFromItem(Blocks.MOSS_BLOCK))
+                .offerTo(exporter, RecipeProvider.getItemPath(mossyResult) + "_from_mossing");
     }
 }
