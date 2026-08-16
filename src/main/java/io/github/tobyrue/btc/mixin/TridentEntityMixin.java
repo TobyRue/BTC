299c7a48d6b005f4dcd1991d24ba323bbc9aba0e
TobyRue <76403689+TobyRue@users.noreply.github.com>
2026-08-16 12:34:03 -0600

    trident fix

--- src/main/java/io/github/tobyrue/btc/mixin/TridentEntityMixin.java
+++ src/main/java/io/github/tobyrue/btc/mixin/TridentEntityMixin.java
@@ -1,15 +1,21 @@
 package io.github.tobyrue.btc.mixin;
 
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.TridentEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TridentEntity.class)
 public class TridentEntityMixin {
-    @Inject(method = "age", at = @At("HEAD"))
+
+    @Inject(method = "age", at = @At("HEAD"), cancellable = true)
     public void age(CallbackInfo ci) {
-        //TODO cancel in some way to stop despawn
+        var me = (TridentEntity) (Object) this;
+        if (me.pickupType == PersistentProjectileEntity.PickupPermission.ALLOWED) {
+            ci.cancel();
+        }
     }
 }
